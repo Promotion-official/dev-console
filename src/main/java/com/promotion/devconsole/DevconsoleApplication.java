14d5fc39580f100cc1b500256dad07cd030890bb
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-07-24 19:24:50 +0900

    Initial commit

---
+++ src/main/java/com/promotion/devconsole/DevconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.promotion.devconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevconsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevconsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promotion/devconsole/DevconsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion.devconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevconsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
